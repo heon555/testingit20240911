1306e207cb1f66b3550aaa03be09ec4e0e5eacac
Taeheon Oh <heonandy5@gmail.com>
2024-09-11 21:45:12 +0900

    sixth commit

--- jain.py
+++ jain.py
@@ -4,9 +4,9 @@ start = str(input("안녕하세요. 당신은 누구입니까? "))
 if start == "moon" :
     print(character["quote"])
     for i in range(1, 73) :
-        print("안녕하세요, 저는 대한민국 전 대통령 문재인입니다. 저는 ", i + 1952,"년도에 ", i + 1952, "살이었습니다.")
+        print("안녕하세요, 저는 대한민국 전 대통령 문재인입니다. 저는 ", i + 1952,"년도에 ", i, "살이었습니다.")
         print("사람이 먼저다")
 else :
-    print(...)
+    print("안녕하세요, ", start, "님.")
 
 print("대화가 끝났습니다.")
