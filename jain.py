8839a638010a042115c205e1647bd3da5dbc689d
Taeheon Oh <heonandy5@gmail.com>
2024-09-11 21:07:20 +0900

    third commit

--- jain.py
+++ jain.py
@@ -1,13 +1,10 @@
 character = {"name": "moon", "quote": "human is first"}
-year = 1953
-age = 1
+
 start = str(input("Hellow who are you? "))
 if start == "moon" :
     print(character["quote"])
-    for i in range(1, 72) :
-        print("I am president Jae-in Moon, I was ", age,"years old in ", year)
-        year += 1
-        age += 1
+    for i in range(1, 73) :
+        print("I am president Jae-in Moon, I was ", i,"years old in ", i + 1952)
 else :
     print(...)
 
