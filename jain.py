4211b4a3aab019448052fc85188144500ae8bbe2
Taeheon Oh <heonandy5@gmail.com>
2024-09-11 23:31:53 +0900

    uu

--- jain.py
+++ jain.py
@@ -5,12 +5,9 @@ if start == "moon" :
     print(character["quote"])
     for i in range(1, 73) :
         print("안녕하세요, 저는 대한민국 전 대통령 문재인입니다. 저는 ", i + 1952,"년도에 ", i, "살이었습니다.")
-    print("사람이 먼저다")
+        print("사람이 먼저다")
 else :
     print("안녕하세요, ", start, "님.")
 
 print("대화가 끝났습니다.")
 print("좋은 하루 되세요.")
-print("규진이 신나용~~~")
-print("오태헌 신나용~~~")
-print("나는 대한민국의 김규진이다!")
